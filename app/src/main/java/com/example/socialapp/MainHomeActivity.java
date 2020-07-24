3122ccf0d0b313be298387f15e0e9221ac397fe8
KUS MAURYA <utsav.singh0023@gmail.com>
2020-07-24 11:03:13 +0530

    Utsav

--- app/src/main/java/com/example/socialapp/MainHomeActivity.java
+++ app/src/main/java/com/example/socialapp/MainHomeActivity.java
@@ -8,17 +8,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -26,13 +29,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainHomeActivity extends AppCompatActivity {
+public class MainHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private NavigationView nav_view;
     private DrawerLayout drawer;
     private ActionBarDrawerToggle adt;
@@ -41,22 +42,34 @@ public class MainHomeActivity extends AppCompatActivity {
     private RecyclerView rv;
     private DatabaseReference dref;
     private String list;
+    private FloatingActionButton addPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
 
+        nav_view = findViewById(R.id.nav_view);
         drawer=findViewById(R.id.drawer);
         toolbar=findViewById(R.id.toolbar);
 
 
-
         adt=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open,R.string.close);
         adt.syncState();
+        nav_view.setNavigationItemSelectedListener(this);
+
 
         rv = findViewById(R.id.rview);
         rv.setLayoutManager(new LinearLayoutManager(this));
+        addPost = findViewById(R.id.faddpost);
+
+        addPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainHomeActivity.this, AddUserPostActivity.class);
+                startActivity(intent);
+            }
+        });
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         String userid = auth.getCurrentUser().getEmail().split("@")[0];
@@ -79,26 +92,46 @@ public class MainHomeActivity extends AppCompatActivity {
 
 
     }
-     class MyViewHolder extends RecyclerView.ViewHolder{
-         TextView fname;
-         TextView lname;
-         TextView date;
-         TextView time;
-         ImageView photo;
-         TextView caption;
-         ImageButton like;
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_profile:
+                Intent i = new Intent(this, MyProfileActivity.class);
+                startActivity(i);
+                break;
+            case R.id.posts_list:
+                Intent j = new Intent(this, UserpostActivity.class);
+                startActivity(j);
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "You have clicked Settings", Toast.LENGTH_SHORT).show();
+                break;
 
-         public MyViewHolder(@NonNull View itemView) {
-             super(itemView);
+            case R.id.logout:
+                signOut();
+                break;
+        }
+        return false;
+    }
 
-                 fname=itemView.findViewById(R.id.firstname);
-                 date=itemView.findViewById(R.id.textView3);
-                 time=itemView.findViewById(R.id.textView8);
-                 photo=itemView.findViewById(R.id.postimage);
-                 caption=itemView.findViewById(R.id.postcaption);
-                 like=itemView.findViewById(R.id.likeib);
-         }
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView fname;
+        TextView date;
+        TextView time;
+        ImageView photo;
+        TextView caption;
+        TextView like;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            fname=itemView.findViewById(R.id.firstname);
+            date=itemView.findViewById(R.id.textView3);
+            time=itemView.findViewById(R.id.textView8);
+            photo=itemView.findViewById(R.id.postimage);
+            caption=itemView.findViewById(R.id.postcaption);
+            like=itemView.findViewById(R.id.like_tv);
+        }
      }
 
      class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
@@ -119,6 +152,7 @@ public class MainHomeActivity extends AppCompatActivity {
          @Override
          public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
              Post_model post_model=list.get(position);
+
              holder.date.setText(post_model.getDate());
              holder.time.setText(post_model.getTime());
              holder.caption.setText(post_model.getCaption());
@@ -152,6 +186,9 @@ public class MainHomeActivity extends AppCompatActivity {
                      Log.d("snapsot", " "+p);
                      list.add(p);
                  }
+                 MyAdapter adp = new MyAdapter(list);
+
+                 rv.setAdapter(adp);
              }
 
              @Override
@@ -159,16 +196,22 @@ public class MainHomeActivity extends AppCompatActivity {
                  Toast.makeText(MainHomeActivity.this, "List is not updated due to: "+ error.getMessage(), Toast.LENGTH_SHORT).show();
              }
          });
-
-         if(list.isEmpty()){
-             Log.d("snapsot", " list is empty");
-             Toast.makeText(this, "list is empty", Toast.LENGTH_SHORT).show();
-         }
-
-         for(Post_model p: list)
-             Log.d("ListValue", p.getCaption());
-
          return list;
      }
+
+    private void signOut(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        SharedPreferences sharedPreferences = getSharedPreferences("UserInformation", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("userId", "Guest");
+        editor.putString("password", "Guest");
+        editor.apply();
+
+        auth.signOut();
+        Intent intent = new Intent(this, LoginRegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 
 }
