2839e703b09ad50162cc807a766854dd3d9a2f2e
Shwetasingh06 <40825340+Shwetasingh06@users.noreply.github.com>
2020-07-25 17:32:53 +0530

    shweta

--- app/src/main/java/com/example/socialapp/AddNewActivity.java
+++ app/src/main/java/com/example/socialapp/AddNewActivity.java
@@ -1,14 +1,72 @@
 package com.example.socialapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class AddNewActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+        rv=findViewById(R.id.add_rv);
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+       // MyAdapter adp=new MyAdapter(list);
+       // rv.setAdapter(adp);
     }
+
+    static class MyviewHolder extends RecyclerView.ViewHolder{
+
+        public MyviewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ImageView photo=itemView.findViewById(R.id.add_iv_profile);
+            TextView name=itemView.findViewById(R.id.add_tv_name);
+            Button btn_add=itemView.findViewById(R.id.add_btn_add);
+            Button btn_view=itemView.findViewById(R.id.add_btn_view);
+        }
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyviewHolder>{
+        private List<UserModel> list;
+        MyAdapter(List<UserModel>list){
+            this.list=list;
+        }
+
+        @NonNull
+        @Override
+        public MyviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyviewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
+
+//    private List<UserModel> getlist(){
+//       // return list;
+//    }
 }
