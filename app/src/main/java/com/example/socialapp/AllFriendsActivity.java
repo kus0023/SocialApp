d1d3b1a286a24ab71b6fda3d115768c6a3402024
KUS MAURYA <utsav.singh0023@gmail.com>
2020-07-25 16:58:03 +0530

    final

---
+++ app/src/main/java/com/example/socialapp/AllFriendsActivity.java
@@ -0,0 +1,18 @@
+package com.example.socialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.storage.StorageReference;
+
+public class AllFriendsActivity extends AppCompatActivity {
+
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_friends);
+    }
+}
