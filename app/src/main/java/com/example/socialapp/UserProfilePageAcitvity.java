683aab6c1070bd813ca9d4ef92b84a64a57ad726
Avantikaverma <aviavantikaverma@gmail.com>
2020-07-25 17:13:11 +0530

    view profile page

---
+++ app/src/main/java/com/example/socialapp/UserProfilePageAcitvity.java
@@ -0,0 +1,13 @@
+package com.example.socialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfilePageAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_page_acitvity);
+    }
+}
